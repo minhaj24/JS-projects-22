2d8b2791a01af1e9a2935dd49ed63b870071e7c4
minhaj24 <41062354+minhaj24@users.noreply.github.com>
2022-02-18 21:57:29 +0500

    completed with all required tasks

--- Project-02-SeatBooking/script.js
+++ Project-02-SeatBooking/script.js
@@ -2,16 +2,56 @@
 
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-const count = document.getElementById('count');
-const total = document.getElementById('total');
+const count = document.querySelector('.count');
+const total = document.querySelector('.total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+populateUI();
+
+//get data from local storage to populate ui
+
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if( selectedSeats !== null && selectedSeats.length >0 ){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+
+        const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+        if(selectedMovieIndex !== null) {
+            movieSelect.selectedIndex = selectedMovieIndex ;
+        }
+    }
+}
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex ;
+    }
 
 function updateSelectedCount() {
+
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    console.log(selectedSeats);
+    const selectedSeatsCount = selectedSeats.length;
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    console.log(seatsIndex);
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount*ticketPrice;
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
 }
+
+//save movie data to local storage
+function setMovieData (movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//Event listener for seat and price manipulation
 
 container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') &&
@@ -21,3 +61,15 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 })
+
+// Event listener for movie selector
+
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+})
+
+//initial cost and total count
+
+updateSelectedCount(); 
